aee923c295f6a451985725c7916e01998c616d53
Andreas Verheijen <averheij@student.codam.nl>
2019-09-13 13:06:03 +0200

    exercise 01

---
+++ ex01/ft_recursive_factorial.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_recursive_factorial.c                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: averheij <averheij@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/09/13 13:00:41 by averheij       #+#    #+#                */
+/*   Updated: 2019/09/13 13:05:47 by averheij      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*hend */
+#include <stdio.h>
+
+/*fstart */
+int	 ft_recursive_factorial(int nb)
+{
+	if (nb > 0)
+	{
+		return (nb * ft_recursive_factorial(nb-1));
+	}
+    else
+    {
+        return (1);
+    }
+}
+/*fend */
+
+int	 main(void)
+{
+	int number;
+	number = 8;
+	printf("%d\n", number);
+	printf("%d\n", ft_recursive_factorial(number));
+}
